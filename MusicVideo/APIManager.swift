b44eee888460d0b3da0d5ef2dcbf0b2361e8aadb
Helge Ansgar Fahlbusch <hfahlbusch@tecracer.de>
2016-06-05 14:39:18 +0200

    NSData to JSON conversion

--- MusicVideo/APIManager.swift
+++ MusicVideo/APIManager.swift
@@ -21,6 +21,36 @@ class APIManager {
         let task = session.dataTaskWithURL(url) {
             (data, response, error) -> Void in
             
+            if error != nil {
+                dispatch_async(dispatch_get_main_queue()) {
+                    completion(result: (error!.localizedDescription))
+                }
+            } else {
+                print(data)
+                do {
+                    if let json = try NSJSONSerialization.JSONObjectWithData(data!, options: .AllowFragments) as? [String: AnyObject] {
+                        print(json)
+                        let priority = DISPATCH_QUEUE_PRIORITY_HIGH
+                        dispatch_async(dispatch_get_global_queue(priority, 0)) {
+                            dispatch_async(dispatch_get_main_queue()) {
+                                completion(result: "JSONSerialization successful")
+                            }
+                        }
+                        
+                    }
+                } catch {
+                    dispatch_async(dispatch_get_main_queue()) {
+                        completion(result: "error in NSJSONSerialization")
+                    }
+                }
+            }
+            
+        }
+        
+        /**
+        let task = session.dataTaskWithURL(url) {
+            (data, response, error) -> Void in
+            
             dispatch_async(dispatch_get_main_queue()) {
                 if error != nil {
                     completion(result: (error!.localizedDescription))
@@ -30,7 +60,8 @@ class APIManager {
                 }
             }
         }
-        
+                **/
         task.resume()
+ 
     }
 }
